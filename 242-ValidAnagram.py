ad36df2660c5a2cc816519979b7bf7a6cfbc9e06
nidal567 <81209936+nidal567@users.noreply.github.com>
2024-06-28 22:11:54 +0000

    added algorithm

--- 242-ValidAnagram.py
+++ 242-ValidAnagram.py
@@ -1,8 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # My solution - check the sorted string, if it is not equal,
+        # MY SOLUTION - check the sorted string, if it is not equal,
         # then it is not an anagram. We don't need the base case either
         return ''.join(sorted(s)) == ''.join(sorted(t))
+    
+        #leetcode style, with algorithm
+        #base case: check lengths
+        if len(s) != len(t):
+            return False
+        
+        counter = {}
+
+        for char in s:
+            counter[char] = counter.get(char, 0) + 1
+        
+        for char in t:
+            if char not in counter or counter[char] == 0:
+                return False
+            counter[char] -= 1
+        
+        return True
 
 #tester example
 solution = Solution()
