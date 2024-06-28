298cf9e5d9d0f938f64779746464258067ead090
nidal567 <81209936+nidal567@users.noreply.github.com>
2024-06-28 21:50:25 +0000

    solved Valid Anagram

---
+++ 242-ValidAnagram.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #base case, check lengths
+        #if len(s) != len(t):
+            #return False
+        
+        #can use a hashmap or a dictionary
+        
+        # My solution - check the sorted string, if it is not equal,
+        # then it is not an anagram. We don't need the base case either
+        if ''.join(sorted(s)) != ''.join(sorted(t)):
+            return False
+        else: 
+            return True
+
+#tester example
+solution = Solution()
+s = "danil"
+t = "nidal"
+result = solution.isAnagram(s, t)
+print(result)
