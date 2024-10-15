5d618934e7f9cea3ea3e76fcce2735cf6d2c5e3d
nidal567 <81209936+nidal567@users.noreply.github.com>
2024-10-14 20:35:38 -0400

    Tuple Swap black-white ball

---
+++ 2938-SeparateBlackandWhiteBalls.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        output = 0
+        #string to list because strings are immutable
+        s = list(s)
+
+        for i in range(1, len(s)):
+            #tuple swap
+            if s[i-1] == '1' and s[i] == '0':
+                s[i-1], s[i] = '0', '1'  
+                output += 1
+        return output
+
+"""
+Think about string to list
+s = "110"
+s_list = list(s) #['1', '1', '0']
+s_list[0], s_list[1] = s_list[1], s_list[0] #['0', '1', '1']
+# if required to print out back in string format:
+s_modified = "".join(s_list) # "011"
+"""
