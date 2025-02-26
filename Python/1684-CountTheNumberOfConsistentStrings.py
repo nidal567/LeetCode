a135ee0218ab9112c53c132b726dae5ae39814c0
Nidal Danial <81209936+nidal567@users.noreply.github.com>
2025-02-25 20:56:11 -0500

    Without python specific function

--- Python/1684-CountTheNumberOfConsistentStrings.py
+++ Python/1684-CountTheNumberOfConsistentStrings.py
@@ -3,8 +3,21 @@ class Solution:
         # want to convert to a set for 0(1) lookups - efficient
         allowed = set(allowed)
         count = 0
-
+        
         for word in words:
-            if all(character in allowed for character in word):
+            is_consistent = True
+            for characters in word:
+                if characters not in allowed:
+                    is_consistent = False
+                    break
+            if is_consistent:
                 count += 1
         return count
+
+        #Python specific function: all()
+        """
+        for word in words:
+            if all(character in allowed for character in word):
+                count +=1
+        return count
+        """
