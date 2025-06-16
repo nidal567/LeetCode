1f326f82e6ef8ddd5cd14f1bd62336ea11259b56
Nidal Danial <81209936+nidal567@users.noreply.github.com>
2025-06-16 00:46:52 -0400

    Update HappyNumber

--- Python/202-HappyNumber.py
+++ Python/202-HappyNumber.py
@@ -7,3 +7,21 @@ class Solution:
             n = sum(int(digit) ** 2 for digit in str(n))
         
         return n == 1
+
+"""
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def next_num(num):
+            total = 0
+            while num > 0:
+                digits = num % 10
+                total += digits * digits
+                num = num // 10
+            return total
+        
+        seen = set()
+        while n!= 0 and n not in seen:
+            seen.add(n)
+            n = next_num(n)
+        return n == 1
+"""
